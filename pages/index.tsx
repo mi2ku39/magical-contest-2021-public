568630bd882794b90a24f3c1ba733f6b91086039
DenpaGhost <az.kinosaki@gmail.com>
2021-08-12 23:59:04 +0900

    iconの追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,60 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { IPlayerApp, ISongMap, IVideo, Player } from "textalive-app-api";
+import styles from "@/pages/index.module.scss";
 
 const index: React.FC = () => {
-  const [token] = useState<string>(process.env.NEXT_PUBLIC_TEXTALIVE_APP_TOKEN)
-  const [fallbackSongUrl] = useState<string>(process.env.NEXT_PUBLIC_FALLBACK_SONG_URL)
-  const [player, setPlayer] = useState<Player | null>(null)
+  const [token] = useState<string>(process.env.NEXT_PUBLIC_TEXTALIVE_APP_TOKEN);
+  const [fallbackSongUrl] = useState<string>(
+    process.env.NEXT_PUBLIC_FALLBACK_SONG_URL
+  );
+  const [player, setPlayer] = useState<Player | null>(null);
 
-  const onAppReady = useCallback<(app: IPlayerApp) => void>((app) => {
-    if (!app.songUrl) {
-      player.createFromSongUrl(fallbackSongUrl)
-    }
-  }, [player])
+  const mediaElement = useRef<HTMLDivElement>();
 
-  const onSongMapLoad = useCallback<(songMap?: ISongMap, reason?: Error) => void>((songMap, reason) => {
-    console.dir(songMap)
-  }, [])
+  const onAppReady = useCallback<(app: IPlayerApp) => void>(
+    (app) => {
+      if (!app.songUrl) {
+        player.createFromSongUrl(fallbackSongUrl);
+      }
+    },
+    [player]
+  );
+
+  const onSongMapLoad = useCallback<
+    (songMap?: ISongMap, reason?: Error) => void
+  >((songMap, reason) => {
+    console.dir(songMap);
+  }, []);
 
   const onVideoReady = useCallback<(v?: IVideo) => void>((v) => {
-    if (!v) return
-
-    console.dir(v.firstPhrase)
-  }, [])
+    if (!v) return;
+    console.dir(v.firstPhrase);
+  }, []);
 
   useEffect(() => {
     if (!player) {
-      setPlayer(new Player(
-        {
+      setPlayer(
+        new Player({
           app: {
-            token
-          }
-        }
-      ))
+            token,
+          },
+          mediaElement: mediaElement.current,
+        })
+      );
     } else {
       player.addListener({
         onAppReady,
         onSongMapLoad,
-        onVideoReady
-      })
+        onVideoReady,
+      });
     }
-  }, [token, player, onAppReady, onSongMapLoad, onVideoReady])
+  }, [token, player, mediaElement, onAppReady, onSongMapLoad, onVideoReady]);
 
-  return <div>hello</div>;
+  return (
+    <div>
+      <img src="/images/icons/play.svg" />
+      <div ref={mediaElement} className={styles.media}></div>
+    </div>
+  );
 };
 export default index;
