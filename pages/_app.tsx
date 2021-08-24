d93d68e3a5f39a6bf14b774312e4814bf561275a
DenpaGhost <az.kinosaki@gmail.com>
2021-08-24 17:36:13 +0900

    グローバルのスタイルシート追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import React from "react";
+import "@/styles/css/main.scss";
 
 const app = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
