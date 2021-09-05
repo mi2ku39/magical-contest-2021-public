b0dad3eb0d809fd757b304afbbbcda09e58f4138
DenpaGhost <az.kinosaki@gmail.com>
2021-09-05 18:36:55 +0900

    定数用オブジェクト作成

---
+++ constants/Icon.ts
@@ -0,0 +1,11 @@
+const basePath = "/images/icons";
+
+export default {
+  replay: `${basePath}/replay.svg`,
+  pause: `${basePath}/pause.svg`,
+  play: `${basePath}/play.svg`,
+  volumeOff: `${basePath}/volume_off.svg`,
+  volumeDown: `${basePath}/volume_down.svg`,
+  volumeUp: `${basePath}/volume_up.svg`,
+  volumeMinimum: `${basePath}/volume_minimum.svg`,
+};
