a138d2d6e204bc001f787fc8467a11e51a4025e1
DenpaGhost <az.kinosaki@gmail.com>
2021-09-22 17:47:41 +0900

    Icon変更

--- constants/Icon.ts
+++ constants/Icon.ts
@@ -2,14 +2,14 @@ const basePath = "/images/icons";
 
 export default {
   close: `${basePath}/close.svg`,
-  ghostserver: `${basePath}/gs.svg`,
   info: `${basePath}/info.svg`,
   pause: `${basePath}/pause.svg`,
   play: `${basePath}/play.svg`,
   replay: `${basePath}/replay.svg`,
-  volumeDown: `${basePath}/volume_down.svg`,
-  volumeMinimum: `${basePath}/volume_minimum.svg`,
-  volumeOff: `${basePath}/volume_off.svg`,
-  volumeUp: `${basePath}/volume_up.svg`,
+  volumeDown: `${basePath}/volume-down.svg`,
+  volumeMinimum: `${basePath}/volume-minimum.svg`,
+  volumeOff: `${basePath}/volume-off.svg`,
+  volumeUp: `${basePath}/volume-up.svg`,
   mediaChange: `${basePath}/media-change.svg`,
+  musicNote: `${basePath}/music-note.svg`,
 };
