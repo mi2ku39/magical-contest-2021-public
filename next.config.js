391689604a5de4d2940aa97c690336d10d320a79
DenpaGhost <az.kinosaki@gmail.com>
2021-09-26 22:09:49 +0900

    ベースパスを設定

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 module.exports = {
-    env: {
-        NEXT_PUBLIC_TEXTALIVE_APP_TOKEN: process.env.TEXTALIVE_APP_TOKEN,
-        NEXT_PUBLIC_FALLBACK_SONG_URL: process.env.FALLBACK_SONG_URL
-    }
-}
+  basePath: process.env.NODE_ENV === "production" ? "/docs" : "",
+  env: {
+    NEXT_PUBLIC_TEXTALIVE_APP_TOKEN: process.env.TEXTALIVE_APP_TOKEN,
+    NEXT_PUBLIC_FALLBACK_SONG_URL: process.env.FALLBACK_SONG_URL,
+  },
+};
