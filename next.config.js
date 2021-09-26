5978347c9a098b9fbd6e8d180897f5f427757fc5
DenpaGhost <az.kinosaki@gmail.com>
2021-09-26 22:14:55 +0900

    ベースパス名変更

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 module.exports = {
-  basePath: process.env.NODE_ENV === "production" ? "/docs" : "",
+  basePath:
+    process.env.NODE_ENV === "production" ? "/magical-contest-2021" : "",
   env: {
     NEXT_PUBLIC_TEXTALIVE_APP_TOKEN: process.env.TEXTALIVE_APP_TOKEN,
     NEXT_PUBLIC_FALLBACK_SONG_URL: process.env.FALLBACK_SONG_URL,
