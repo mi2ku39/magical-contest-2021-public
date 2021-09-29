f9e72a81ee102f5306e951f9f5b51fb8d9c96d19
DenpaGhost <az.kinosaki@gmail.com>
2021-09-29 18:02:19 +0900

    faviconのパス修正

--- components/OGP/Ogp.tsx
+++ components/OGP/Ogp.tsx
@@ -9,7 +9,7 @@ const Ogp: React.FC = () => {
       <link
         rel="shortcut icon"
         type="image/x-icon"
-        href={`${basePath}/images/favicon.ico`}
+        href={`${basePath}/favicon.ico`}
       />
       <meta name="viewport" content="width=device-width,initial-scale=1.0" />
       <meta name="twitter:card" content="summary_large_image" />
